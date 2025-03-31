4ba1d8387918f355fd65c82efea0307cea1a9ded
CCS-WIN-112\Haruki Kimura <harukimura0317@gmail.com>
2025-03-31 20:41:02 +0900

    似た処理、変数が多かったため、スプライトアニメーション用の基底クラスを作成

--- training-develop/Assets/@Training/Scripts/1_Play/HeartAnimation.cs
+++ training-develop/Assets/@Training/Scripts/1_Play/HeartAnimation.cs
@@ -1,73 +1,21 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
 /// <summary>
 /// 体力アニメーションクラス
 /// </summary>
-public class HeartAnimation : MonoBehaviour
+public class HeartAnimation : SpriteAnimation
 {
-    /// <summary>
-    /// 画像編集用コンポーネント
-    /// </summary>
-    Image IMGHeart;
-
     [SerializeField, Header("体力を確認するプレイヤー")]
     PlayerComponent Player;
 
     [SerializeField, Header("担当するHP数")]
     int HP;
 
-    [SerializeField, Header("ハート(破壊)画像")]
-    Sprite[] HeartBreak;
-
-    /// <summary>
-    /// アニメーション間隔を管理する変数
-    /// </summary>
-    float intervalAnimation;
-
-    /// <summary>
-    /// 現在の画像の添字
-    /// </summary>
-    uint BreakHeartAnimationIndex;
-
-    [SerializeField, Header("アニメーション間隔")]
-    float IntervalAnimationMax;
-
-    void Start()
-    {
-        // 画像の初期化
-        IMGHeart = GetComponent<Image>();
-        BreakHeartAnimationIndex = 0;
-        IMGHeart.sprite = HeartBreak[BreakHeartAnimationIndex];
-
-        // アニメーション間隔の初期化
-        intervalAnimation = 0f;
-    }
-
     void Update()
     {
+        // プレイヤーのHPが担当するHP数を下回ったらハートが壊れるアニメーションを再生
         if (Player.HitPoint < HP) {
-            AnimationHeart();
+            Animation();
         }
-    }
-
-    /// <summary>
-    /// ハートのアニメーションを行う
-    /// </summary>
-    void AnimationHeart()
-    {
-        if (intervalAnimation < IntervalAnimationMax) {
-            // アニメーションのインターバル中
-            intervalAnimation += Time.deltaTime;
-            return;
-        }
-
-        if (BreakHeartAnimationIndex == HeartBreak.Length - 1) {
-            Destroy(gameObject);
-        } else {
-            BreakHeartAnimationIndex++;
-            IMGHeart.sprite = HeartBreak[BreakHeartAnimationIndex];
-        }
-        intervalAnimation = 0f;
     }
 }
--- training-develop/Assets/@Training/Scripts/1_Play/HitEffectAnimation.cs
+++ training-develop/Assets/@Training/Scripts/1_Play/HitEffectAnimation.cs
@@ -1,63 +1,10 @@
-﻿using UnityEngine;
-
+﻿
 /// <summary>
 /// ヒットエフェクトのアニメーションクラス
 /// </summary>
-public class HitEffectAnimation : MonoBehaviour
+public class HitEffectAnimation : SpriteAnimation
 {
-    /// <summary>
-    /// 画像編集用コンポーネント
-    /// </summary>
-    SpriteRenderer hitEffect;
-
-    [SerializeField, Header("ヒットエフェクト画像")]
-    Sprite[] Explosion;
-
-    /// <summary>
-    /// アニメーション間隔を管理する変数
-    /// </summary>
-    float intervalAnimation;
-
-    [SerializeField, Header("アニメーション間隔")]
-    float IntervalAnimationMax = 0.1f;
-
-    /// <summary>
-    /// 現在の画像の添字
-    /// </summary>
-    uint effectIndex;
-
-    void Start()
-    {
-        // 画像の初期化
-        hitEffect = GetComponent<SpriteRenderer>();
-        effectIndex = 0;
-        hitEffect.sprite = Explosion[effectIndex];
-
-        // アニメーション間隔の初期化
-        intervalAnimation = 0f;
-    }
-
+    // ヒットエフェクトのアニメーションを再生
     void Update()
-        => AnimationHitEffect();
-
-    /// <summary>
-    /// ヒットエフェクトのアニメーションを行う
-    /// </summary>
-    void AnimationHitEffect()
-    {
-        if (intervalAnimation < IntervalAnimationMax) {
-            // アニメーションのインターバル中
-            intervalAnimation += Time.deltaTime;
-            return;
-        }
-
-        // アニメーション
-        if (effectIndex == Explosion.Length - 1) {
-            Destroy(gameObject);
-        } else {
-            effectIndex++;
-            hitEffect.sprite = Explosion[effectIndex];
-        }
-        intervalAnimation = 0f;
-    }
+        => Animation();
 }
